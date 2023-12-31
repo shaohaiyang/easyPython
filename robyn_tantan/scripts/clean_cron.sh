398c5951184fb27284e81ec68319c74f82615452
shaohaiyang <shaohaiyang@gmail.com>
2024-01-01 00:50:29 +0800

    Update clean_cron.sh with bugfixed

--- robyn_tantan/scripts/clean_cron.sh
+++ robyn_tantan/scripts/clean_cron.sh
@@ -3,12 +3,12 @@ daytime=$(date +%s)
 
 echo "now time: $daytime "
 for time in $(crontab  -l | awk '/^[^#]/{print $NF}');do
+  cron_tmp=$(crontab  -l | awk '/'"$time"'/{print $(NF-2)}')
   echo -en "cron task time: $time | "
   if [ $daytime -gt $time ];then
-    cron_tmp=$(crontab  -l | awk '/'"$time"'/{print $(NF-2)}')
     echo "--- task: $cron_tmp"
-    rm -rf $cron_tmp
-    crontab  -l | grep -vE "$time|^$" | crontab
+    #rm -rf $cron_tmp
+    #crontab  -l | grep -vE "$time|^$" | crontab
   else
     echo "+++ task: $cron_tmp"
   fi
