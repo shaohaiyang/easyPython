eb87a27452e7c0006ac04e4bc324d6b5783a17af
shaohaiyang <shaohaiyang@gmail.com>
2024-01-06 23:21:25 +0800

    Copy todo app to robyn_demo with docker, remove robyn_todo redundant files

--- robyn_todo/docker_cmd.sh
+++
@@ -1,3 +0,0 @@
-#docker build -t robyn_todo --rm .
-docker run -tid --name=robyn_todo -v /root/robyn_todo/src:/app -p 4000:8080 \
-      robyn_todo sh -c "prisma db push --schema /app/schema.prisma && python3 /app/app.py"
